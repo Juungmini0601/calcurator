bffe856c7c03049bd6aa91be279c55ba939b1cf9
김정민 <raon37910@gmail.com>
2025-02-25 16:42:20 +0900

    [refactor] 입출력 클래스 분리

---
+++ src/main/java/com/raon/middle/io/ConsoleReader.java
@@ -0,0 +1,66 @@
+package com.raon.middle.io;
+
+import java.util.Scanner;
+
+import com.raon.middle.command.Commands;
+import com.raon.middle.operator.Operators;
+
+/**
+ * @author    : kimjungmin
+ * Created on : 2025. 2. 25.
+ */
+public class ConsoleReader {
+
+	private static final Scanner sc = new Scanner(System.in);
+
+	private static class Validator {
+		private static final Operators operators = new Operators();
+		private static final Commands commands = new Commands();
+
+		private static void validateInputNumber(int num) {
+			if (num < 0) {
+				System.out.println("0 이상의 정수를 입력 해주세요");
+				System.exit(-1);
+			}
+		}
+
+		private static void validateInputOperator(char operator) {
+			if (!operators.contains(operator)) {
+				throw new IllegalArgumentException("연산자를 정확히 입력 해주세요!");
+			}
+		}
+
+		private static void validateCommand(String command) {
+			if (!commands.isCommand(command)) {
+				throw new IllegalArgumentException("올바른 명령어가 아닙니다.");
+			}
+		}
+	}
+
+	public String readCommand() {
+		System.out.print("명령어 입력: ");
+		String command = sc.next();
+
+		Validator.validateCommand(command);
+
+		return command;
+	}
+
+	public int readInt() {
+		System.out.print("숫자 입력(0 이상의 정수): ");
+		int num1 = sc.nextInt();
+
+		Validator.validateInputNumber(num1);
+
+		return num1;
+	}
+
+	public char readOperator() {
+		System.out.print("사칙연산 기호를 입력하세요 (+, -, *, /): ");
+
+		char operator = sc.next().charAt(0);
+		Validator.validateInputOperator(operator);
+
+		return operator;
+	}
+}
---
+++ src/main/java/com/raon/middle/io/ConsoleWriter.java
@@ -0,0 +1,19 @@
+package com.raon.middle.io;
+
+/**
+ * @author    : kimjungmin
+ * Created on : 2025. 2. 25.
+ */
+public class ConsoleWriter {
+
+	public void printResult(int num1, int num2, char operator, int result) {
+		System.out.printf("%d %c %d = %d\n", num1, operator, num2, result);
+	}
+
+	public void showCommands() {
+		System.out.println("=============== 명령어 목록 =============");
+		System.out.println("1. calc: 계산하기");
+		System.out.println("2. remove: 가장 먼저 저장된 데이터 삭제");
+		System.out.println("3. exit: 종료");
+	}
+}
