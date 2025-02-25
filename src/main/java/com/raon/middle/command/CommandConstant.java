e174ed1f019bade9a8e20c670f54d9efe51e4bd7
김정민 <raon37910@gmail.com>
2025-02-25 16:41:44 +0900

    [feature] 명령어 상수 및 리스트 추가

---
+++ src/main/java/com/raon/middle/command/CommandConstant.java
@@ -0,0 +1,11 @@
+package com.raon.middle.command;
+
+/**
+ * @author    : kimjungmin
+ * Created on : 2025. 2. 25.
+ */
+public class CommandConstant {
+	public static final String CALC = "calc";
+	public static final String REMOVE = "remove";
+	public static final String EXIT = "exit";
+}
---
+++ src/main/java/com/raon/middle/command/Commands.java
@@ -0,0 +1,17 @@
+package com.raon.middle.command;
+
+import static com.raon.middle.command.CommandConstant.*;
+
+import java.util.List;
+
+/**
+ * @author    : kimjungmin
+ * Created on : 2025. 2. 25.
+ */
+public class Commands {
+	private static final List<String> commands = List.of(CALC, REMOVE, EXIT);
+
+	public boolean isCommand(String command) {
+		return commands.contains(command);
+	}
+}
