83299baf788e8184877094be9f7b31748baa0bc3
김정민 <raon37910@gmail.com>
2025-02-25 15:54:53 +0900

    [feature] 사칙연산 기호 입력 기능 추가

--- src/main/java/com/raon/basic/App.java
+++ src/main/java/com/raon/basic/App.java
@@ -1,6 +1,8 @@
 package com.raon.basic;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
+import java.util.Set;
 
 /**
  * @author    : kimjungmin
@@ -10,22 +12,48 @@ import java.util.Scanner;
  */
 public class App {
 
+	private static final char ADD = '+';
+	private static final char MINUS = '-';
+	private static final char MULTIPLY = '*';
+	private static final char DIVIDE = '/';
+
+	private static final Set<Character> OPERATORS = Set.of(ADD, MINUS, MULTIPLY, DIVIDE);
+
 	private static final Scanner sc = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		System.out.print("첫 번째 숫자 입력(0 이상의 정수): ");
-		int num1 = sc.nextInt();
-		validate(num1);
+		try {
+			System.out.print("첫 번째 숫자 입력(0 이상의 정수): ");
+			int num1 = sc.nextInt();
+			validateInputNumber(num1);
 
-		System.out.print("두 번째 숫자 입력(0 이상의 정수): ");
-		int num2 = sc.nextInt();
-		validate(num2);
+			System.out.print("두 번째 숫자 입력(0 이상의 정수): ");
+			int num2 = sc.nextInt();
+			validateInputNumber(num2);
 
-		System.out.println("숫자1: " + num1);
-		System.out.println("숫자2: " + num2);
+			System.out.print("사칙연산 기호를 입력하세요 (+, -, *, /): ");
+
+			char operator = sc.next().charAt(0);
+			validateInputOperator(operator);
+
+			System.out.println("숫자1: " + num1);
+			System.out.println("숫자2: " + num2);
+			System.out.println("연산자: " + operator);
+		} catch (InputMismatchException | IllegalArgumentException e) {
+			System.out.println("입력값을 다시 확인 해주세요!");
+		} catch (Exception e) {
+			System.out.println("예상하지 못한 예외가 발생했습니다.");
+			throw e;
+		}
 	}
 
-	public static void validate(int num) {
+	private static void validateInputOperator(char operator) {
+		if (!OPERATORS.contains(operator)) {
+			throw new IllegalArgumentException("연산자를 정확히 입력 해주세요!");
+		}
+	}
+
+	private static void validateInputNumber(int num) {
 		if (num < 0) {
 			System.out.println("0 이상의 정수를 입력 해주세요");
 			System.exit(-1);
