de24d2b7182c59ebe328180a2b5e94f7948a3688
김정민 <raon37910@gmail.com>
2025-02-25 10:57:01 +0900

    Initial Commit

---
+++ src/main/java/com/raon/Main.java
@@ -0,0 +1,13 @@
+package com.raon;
+
+/**
+ * @author    : kimjungmin
+ * Created on : 2025. 2. 25.
+ */
+public class Main {
+	public static void main(String[] args) {
+		// 현재 실행 중인 Java 버전 출력
+		String javaVersion = System.getProperty("java.version");
+		System.out.println("현재 Java 버전: " + javaVersion);
+	}
+}
