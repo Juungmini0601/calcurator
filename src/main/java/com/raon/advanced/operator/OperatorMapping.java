fa43e7a082b907b00cc63de86cee9f9d5ed54bf3
김정민 <raon37910@gmail.com>
2025-02-28 19:58:07 +0900

    [refactor] 메소드 참조 람다로 변경
    개인적인 취향..

--- src/main/java/com/raon/advanced/operator/OperatorMapping.java
+++ src/main/java/com/raon/advanced/operator/OperatorMapping.java
@@ -14,10 +14,11 @@ public class OperatorMapping<T extends Number & Comparable<T>> {
 
 	public OperatorMapping(NumberOperator<T> numberOperator) {
 		operatorMapping = Map.of(
-			OperatorConstant.ADD, numberOperator::add,
-			OperatorConstant.MINUS, numberOperator::minus,
-			OperatorConstant.MULTIPLY, numberOperator::multiply,
-			OperatorConstant.DIVIDE, numberOperator::divide
+			// 메소드 참조는 한눈에 잘 안보이는거 같아서 람다로..
+			OperatorConstant.ADD, (num1, num2) -> numberOperator.add(num1, num2),
+			OperatorConstant.MINUS, (num1, num2) -> numberOperator.minus(num1, num2),
+			OperatorConstant.MULTIPLY, (num1, num2) -> numberOperator.multiply(num1, num2),
+			OperatorConstant.DIVIDE, (num1, num2) -> numberOperator.divide(num1, num2)
 		);
 	}
 
